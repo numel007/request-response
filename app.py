6462b0cbcaaf540a15fc907ba4f8fc3f161abc44
Jackie <taekwondo.chan@gmail.com>
2020-10-21 17:38:23 -0700

    favorite animal

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+#app.py
+from flask import Flask
+app = Flask(__name__)
 
-# TODO: Follow the assignment instructions to complete the required routes!
-# (And make sure to delete this TODO message when you're done!)
+@app.route('/')
+def homepage():
+    return "Hello World!"
+
+@app.route('/penguins')
+def penguins():
+    return "Penguins are cute!"
+
+@app.route('/fav-animal')
+def fav_animal():
+    return "Cats are obviously the best animal."
+
+if __name__ == '__main__':
+    app.run(debug=True)
